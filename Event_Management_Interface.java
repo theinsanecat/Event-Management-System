e1bb5ada5c954e48b8c05e9e2c9b99784d32f999
Soumya Dwivedi <60401428+theinsanecat@users.noreply.github.com>
2020-08-25 17:21:53 +0530

    Update Event_Management_Interface.java

--- Event_Management_Interface.java
+++ Event_Management_Interface.java
@@ -1,6 +1,18 @@
 import java.util.*;
+
 public interface Event_Management_Interface {
 
 	void displayData(ArrayList<Event> l);
-	
+
+	void inputEvent(Scanner sc, ArrayList<Event> list);
+
+	void deleteEvent(Scanner sc, ArrayList<Event> list);
+
+	void searchEvent(Scanner sc, ArrayList<Event> list);
+
+	void changeFootfall(Scanner sc, ArrayList<Event> list, int u);
+
+	void changeTitle(Scanner sc, ArrayList<Event> list, int u);
+
+	void changeDate(Scanner sc, ArrayList<Event> list, int u);
 }
