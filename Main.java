2e5e68d33c2da1f9ff31b47a19b80431585caeb7
Soumya Dwivedi <60401428+theinsanecat@users.noreply.github.com>
2020-08-25 17:22:39 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,142 +1,77 @@
-import java.util.*;
-
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
-	 
 
-	public static void main(String[] args)
-	{
-		
+	public static void main(String[] args) {
+		EventManipulation x = new EventManipulation();
 		Scanner sc = new Scanner(System.in);
-		//String[] array=new String[5];
-		
 		ArrayList<Event> list = new ArrayList<Event>();
-		
-		
-		while(true)
-		{
-		System.out.println("****************************************************************");	
-		System.out.println("Enter the choice:");
-		System.out.println("1) Enter an event"+"\n2) List all events" + "\n3) Update an event" + "\n4) Delete an event" + "\n5) Search event");
-        int choice=sc.nextInt();
-        switch(choice)
-        {
-        case 1: System.out.println("How many Events you want to register?");
-                int entries=sc.nextInt();
-                
-                for(int i=0;i<entries;i++)
-                { 
-                	Event eve = new Event();
-                	System.out.println("Enter the Event Id:");
-                    int newid=sc.nextInt();
-                    eve.setId(newid);
-                    System.out.println("Enter the Title:");
-                    String newtit=sc.next();
-                    eve.setTitle(newtit);
-                    System.out.println("Enter the Footfall:");
-                    int f=sc.nextInt();
-                    eve.setNo_of_people(f);
-                    System.out.println("Enter the number of Artist/Artists:");
-                    int a=sc.nextInt();
-                    System.out.println("Enter the Artist/Artists:");
-                    String[] newart=new String[a];
-                    for(int j=0;j<a;j++)
-                    {
-                    newart[j]=sc.next();
-                    }
-                    eve.setArtist(newart);
-                    System.out.println("Enter the date(dd-mm-yy):");
-                    String newdate=sc.next();
-                    eve.setDate(newdate);
-                    array=newart;
-                    list.add(eve);
-                	
-                }
-                
-                
-               
-                break;
-        
-        
-        case 2: for(int u=0;u<list.size();u++)
-        {
-               list.get(u).displayData(list);
-        }
-        	break;
-        
-        case 3: System.out.println("Enter the Id of the event you want to change :");
-                int editId = sc.nextInt();
-                for(int u=0;u<list.size();u++)
-                {
-                if(editId==list.get(u).getId())
-                {
-//                	int index=list.indexOf(eve);
-                	
-                	System.out.println("Which part you want to change?:"+"\n1. Id"+"\n2. Title"+"\n3. Footfall"+"\n4. Date"+"\nEnter your choice :");
-                	int choice1=sc.nextInt();
-                	Event temp=new Event();
-                	switch(choice1)
-                	{
-                	case 1: System.out.println("Enter the new Id:");
-                	        int changeid=sc.nextInt();                	        
-                	        list.get(u).setId(changeid);
-                	        temp.displayData(list);break;
-                	    
-                	case 2: System.out.println("Enter the new Title:");
-        	        		String changetitle=sc.next();
-        	                list.get(u).setTitle(changetitle);
-        	        		temp.displayData(list);break;
-        	        		
-//        	        		
-                	case 3: System.out.println("Enter the new Footfall:");			
-                			int changefootfall=sc.nextInt();
-                			list.get(u).setNo_of_people(changefootfall);
-        	        		temp.displayData(list);
-                			break;
-                			
-                	case 4: System.out.println("Enter the new Date(dd-mm-yy):");
-                			String changedate=sc.next();
-                			list.get(u).setDate(changedate);
-        	        		temp.displayData(list);
-                		
-                			break;
-                		
-                	}
-                	
-                	
-                	
-                }}break;
-                
-       case 4: System.out.println("Enter the Event Id you want to delete:");
-               int delevent=sc.nextInt();
-               for(int u=0;u<list.size();u++)
-               if(delevent==list.get(u).getId())
-               {
-               	 list.remove(list.get(u));
-               }break;
-               
-               
-       case 5: System.out.println("Enter anything from(Id,Title,Footfall) for the Event you want to search:");
-               String ans= sc.next();
-               for(int u=0;u<list.size();u++)
-               {
-            	 Integer searchfootfall=list.get(u).getNo_of_people();
-            	 String searchtit=list.get(u).getTitle();  
-            	 Integer var=list.get(u).getId();  
-               if(ans.equals(var.toString()))
-            	   list.get(u).display();
-               else if(ans.equals(searchtit))
-            	   list.get(u).display();
-               else if(ans.equals(searchfootfall.toString()))
-            	   list.get(u).display();
-               
-            	  
-               }break;
 
-        
-        }
-        
-    }
-		
+		while (true) {
+			int choice = mainMenu(sc);
+			switch (choice) {
+			case 1:
+				x.inputEvent(sc, list);
+
+				break;
+
+			case 2:
+				System.out.println("\n*************************All Events*************************\n");
+
+				x.displayData(list);
+				break;
+
+			case 3:
+				System.out.println("Enter the Id of the event you want to change :");
+				int editId = sc.nextInt();
+				for (int u = 0; u < list.size(); u++) {
+					if (editId == list.get(u).getId()) {
+
+						System.out.println("Which part you want to change?:" + "\n1. Title" + "\n2. Footfall"
+								+ "\n3. Date" + "\nEnter your choice :");
+						int choice1 = sc.nextInt();
+						switch (choice1) {
+						case 1:
+							// Updates an exiting Event Title to a new one
+							x.changeTitle(sc, list, u);
+							break;
+						case 2:
+							// Updates the count of people attending the Event
+							x.changeFootfall(sc, list, u);
+							break;
+						case 3:
+							// Updates the date of the Event
+							x.changeDate(sc, list, u);
+							list.get(u).display(list);
+							break;
+						}
+					}
+				}
+				break;
+
+			case 4:
+				// Deletes an Event
+				x.deleteEvent(sc, list);
+				break;
+
+			case 5:
+				// Searches an Event in the event list for corresponding values of ID/Title/Footfall 
+				x.searchEvent(sc, list);
+				break;
+			}
+
+		}
+
 	}
+
+	private static int mainMenu(Scanner sc) {
+		System.out.println("\n*.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.*");
+		System.out.println("\t\t\tEnter the Choice:");
+		System.out.println("\n1) Enter an Event" + "\n2) List all Events" + "\n3) Update an Event"
+				+ "\n4) Delete an Event" + "\n5) Search Event");
+		int choice = sc.nextInt();
+		return choice;
+	}
+
 }
